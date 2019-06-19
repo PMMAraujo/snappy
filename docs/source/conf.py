2f8ea30a91221eda4e51e53fbf8974f3e8bcad4d
PMMAraujo <pedro10araujo@gmail.com>
2019-06-19 14:11:44 +0100

    changed version in documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Pedro Araujo'
 author = 'Pedro Araujo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
